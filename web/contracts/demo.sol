f5ed336eaf7154e0461db8b422dac43350bfa4d6
Aditya <aditya.rout@beatoapp.com>
2022-03-26 20:15:39 +0530

    first D App

---
+++ web/contracts/demo.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.12;
+
+contract demo {
+    uint number;
+
+    function set(uint _number) public {
+        number = _number;
+    }
+
+    function get() public view returns(uint) {
+        return number;
+    }
+}
